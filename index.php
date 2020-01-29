5a4b4ff687a8286a6890d242c20882b49c89c1c3
Luciano Remes <cybergenik@gmail.com>
2020-01-28 21:31:34 -0700

    hello

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <head> Luciano Remes </head>
   <body>
     <h1>Luciano Remes 2470 Home page:</h1>
-    <p>InClass2: <a href="inclass2/JsonAjax.php">Happy Birthday</a></p>
+    <p>InClass2: <a href="inclass2/jsonajax.php">In CLass 2</a></p>
   </body>
 </html>
