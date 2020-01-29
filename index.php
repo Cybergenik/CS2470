54ae6dc5335bd603efae4dd609e760cafec15779
Luciano Remes <cybergenik@gmail.com>
2020-01-28 18:55:37 -0700

    inclass 2

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head> Luciano Remes </head>
+  <body>
+    <h1>Luciano Remes 2470 Home page:</h1>
+    <p>InClass2: <a href="inclass2/JsonAjax.php">Happy Birthday</a></p>
+  </body>
+</html>
