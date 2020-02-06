a5e552863d4328ae3fe551cc0f63e09b6af0179a
Luciano Remes <cybergenik@gmail.com>
2020-02-05 20:25:00 -0700

    stuff:

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head> <h1>Luciano Remes</h1> 
-  <link rel="stylesheet" href="css/main.css">
+  <head> 
+    <Title>Luciano Reme</Title> 
+    <link rel="stylesheet" href="css/main.css">
   </head>
-  <body>
+  <body id="bg">
     <h1>Luciano Remes 2470 Home page:</h1>
     <p>InClass2: <a class="mybutton" href="inclass2/jsonajax.php">In CLass 2</a></p>
     <p>InClass3: <a class="mybutton" href="inclass3/FamilyAndFriend.php">In CLass 3</a></p>
