744bbe913693f6ad8ab76aa618153d63cde62eb8
Luciano Remes <cybergenik@gmail.com>
2020-02-05 20:28:39 -0700

    stuff

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
   <body id="bg">
     <h1>Luciano Remes 2470 Home page:</h1>
     <p>InClass2: <a class="mybutton" href="inclass2/jsonajax.php">In CLass 2</a></p>
-    <p>InClass3: <a class="mybutton" href="inclass3/FamilyAndFriend.php">In CLass 3</a></p>
+    <p>InClass3: <a class="mybutton" href="inclass3/FamilyAndFriend.php">AJAX/JSON Friends and Family</a></p>
   </body>
 </html>
