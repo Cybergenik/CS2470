fe1f8f7d5378b1a934615cbecbc1903bb66527c9
Luciano Remes <cybergenik@gmail.com>
2020-03-01 02:35:22 -0700

    bettering index navigation

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
     <h1>Luciano Remes 2470 Home page:</h1>
     <p>InClass2: <a class="mybutton" href="inclass2/jsonajax.php">In CLass 2</a></p>
     <p>Assignment 1: <a class="mybutton" href="inclass3/FamilyAndFriend.php">AJAX/JSON Friends and Family</a></p>
+    <p>Assignment 2: <a class="mybutton" href="inclass4/Carousel.php">Carousel using JQuery</a></p>
+    <p>Assignment 3: <a class="mybutton" href="inclass5/JQueryIntroduction.php">Full JQuery site0</a></p>
+    <p>Assignment 4: <a class="mybutton" href="inclass6/AnimateExclean.php">Full JQuery site1</a></p>
   </body>
 </html>
